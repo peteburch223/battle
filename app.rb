d6caf88c53c0f97a3ba5dac19f06ec10b851c247
Misa Ogura <misa.ogura01@gmail.com>
2016-03-15 15:21:37 +0000

    Complete #19

--- app.rb
+++ app.rb
@@ -2,13 +2,22 @@ require 'sinatra/base'
 require 'sinatra'
 
 class Battle < Sinatra::Base
+
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @user_1 = params[:user_1]
-    @user_2 = params[:user_2]
+    session[:user_1] = params[:user_1]
+    session[:user_2] = params[:user_2]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @user_1 = session[:user_1]
+    @user_2 = session[:user_2]
     erb(:play)
   end
 
