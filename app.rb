30145a3e5980919a4207a820a00941b6843fbb9a
Misa Ogura <misa.ogura01@gmail.com>
2016-03-15 14:38:32 +0000

    Complete #18

--- app.rb
+++ app.rb
@@ -1,16 +1,16 @@
 require 'sinatra/base'
 require 'sinatra'
 
+class Battle < Sinatra::Base
+  get '/' do
+    erb(:index)
+  end
 
-get '/' do
-  'Hello Battle World!!'
+  post '/names' do
+    @name = params[:name]
+    erb(:play)
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 end
-
-# class Battle < Sinatra::Base
-#   get '/' do
-#     'Hello Battle World!!'
-#   end
-#
-#   # start the server if ruby file executed directly
-#   run! if app_file == $0
-# end
--- spec/features/fill_in_names_spec.rb
+++ spec/features/fill_in_names_spec.rb
@@ -1,10 +1,12 @@
 RSpec.feature "User Entry", :type => :feature do
-  scenario "User enters name" do
+
+ let(:test_user) { "test user"}
+  scenario "User enters name and page displays the name" do
     visit "/"
 
-    fill_in "name", :with => "test user"
+    fill_in "name", :with => test_user
     click_button "Submit"
 
-    expect(page).to have_text("test user")
+    expect(page).to have_text(test_user)
   end
 end
