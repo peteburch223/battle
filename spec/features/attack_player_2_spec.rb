56704b367c915d8da89a710ab8a217b44098626b
Pete Burch <peteburch223@gmail>
2016-03-16 16:04:48 +0000

    #26 not quite as completed as believed

--- spec/features/attack_player_2_spec.rb
+++ spec/features/attack_player_2_spec.rb
@@ -1,23 +1,23 @@
-# RSpec.feature "Attack button: #22", :type => :feature do
-#
-#   let(:player1) { "test user 1"}
-#   let(:player2) { "test user 2"}
-#   let(:player1_hp) { Player::MAX_HP}
-#   let(:player2_hp) { Player::MAX_HP}
-#
-#   before (:each) do
-#     sign_in_and_play(player1, player2)
-#     visit "/play"
-#   end
-#
-#   scenario "Game displays an attack button" do
-#       expect(page).to have_button("Attack")
-#   end
-#
-#   scenario "attacking displays confirmation page with reduced hp" do
-#     click_button("Attack")
-#     expect(page).to have_text("#{player1} (#{player1_hp}) attacked #{player2} (#{player2_hp - Player::DAMAGE})")
-#   end
-#
-#
-# end
+RSpec.feature "Attack button: #22", :type => :feature do
+
+  let(:player1) { "test user 1"}
+  let(:player2) { "test user 2"}
+  let(:player1_hp) { Player::MAX_HP}
+  let(:player2_hp) { Player::MAX_HP}
+
+  before (:each) do
+    sign_in_and_play(player1, player2)
+    visit "/play"
+  end
+
+  scenario "Game displays an attack button" do
+      expect(page).to have_button("Attack")
+  end
+
+  scenario "attacking displays confirmation page with reduced hp" do
+    click_button("Attack")
+    expect(page).to have_text("#{player1} (#{player1_hp}) attacked #{player2} (#{player2_hp - Player::DAMAGE})")
+  end
+
+
+end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,24 +1,24 @@
-# RSpec.feature "Viewing hit points: #20", :type => :feature do
-#
-#   let(:player1) { "test user 1"}
-#   let(:player2) { "test user 2"}
-#   let(:player2_hp) { "23"}
-#
-#   before (:each) do
-#     sign_in_and_play(player1, player2)
-#     visit "/play"
-#   end
-#
-#   scenario "Game displays user 2 hit points as text" do
-#     within("//div[@class='player2_hp']") do
-#       expect(page).to have_content("#{player2} hitpoints: #{Battle::MAX_HP}")
-#     end
-#   end
-#
-#   scenario "Game displays user 2 hit points as progress bar" do
-#     within("//div[@class='player2_hp']") do
-#       expect(page.has_xpath?("//progress[@max='#{Battle::MAX_HP}'][@value='#{Battle::MAX_HP}']"))
-#     end
-#   end
-#
-# end
+RSpec.feature "Viewing hit points: #20", :type => :feature do
+
+  let(:player1) { "test user 1"}
+  let(:player2) { "test user 2"}
+  let(:player2_hp) { "23"}
+
+  before (:each) do
+    sign_in_and_play(player1, player2)
+    visit "/play"
+  end
+
+  scenario "Game displays user 2 hit points as text" do
+    within("//div[@class='player2_hp']") do
+      expect(page).to have_content("#{player2} hitpoints: #{Battle::MAX_HP}")
+    end
+  end
+
+  scenario "Game displays user 2 hit points as progress bar" do
+    within("//div[@class='player2_hp']") do
+      expect(page.has_xpath?("//progress[@max='#{Battle::MAX_HP}'][@value='#{Battle::MAX_HP}']"))
+    end
+  end
+
+end
